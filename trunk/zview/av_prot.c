7387c36d24568e450d70f6df326ed4480cb887f9
mikro_sk <mikro_sk@d0bd7d33-7730-4d7d-97a9-9cea30d9b490>
2013-02-09 22:34:05 +0000

    Windom AvStrfmt is not very helpful. Fix quotes in AV.

--- trunk/zview/av_prot.c
+++ trunk/zview/av_prot.c
@@ -5,6 +5,73 @@
 /* Prototype */
 void va_start( void);
 
+/* taken from mxPlay - simplified version, takes only one argument */
+static char* ParseArgs( char* cmdline )
+{
+	BOOL	inQuote = FALSE;
+	int		i = 0;
+	int		j = 0;
+	char	all[1023+1];
+
+	while( cmdline[i] != '\0' )
+	{
+		if( cmdline[i] != '\'' && cmdline[i] != ' ' && inQuote == FALSE )
+		{
+			/* simple file (not in quotes) */
+			while( cmdline[i] != '\0' && cmdline[i] != ' ' )
+			{
+				all[j++] = cmdline[i++];
+			}
+
+			all[j++] = '\0';
+			strcpy( cmdline, all );
+			return cmdline;
+		}
+
+		switch( cmdline[i] )
+		{
+			case '\'':
+				i++;
+				if( inQuote == TRUE )
+				{
+					if( cmdline[i] == '\'' )					/* '' -> ' */
+					{
+						all[j++] = '\'';
+						i++;
+					}
+					else
+					{
+						inQuote = FALSE;
+						all[j++] = '\0';
+						strcpy( cmdline, all );
+						return cmdline;
+					}
+				}
+				else
+				{
+					inQuote = TRUE;
+				}
+				break;
+
+			case ' ':
+				if( inQuote == TRUE )
+				{
+					all[j++] = cmdline[i++];
+				}
+				else
+				{
+					i++;
+				}
+				break;
+
+			default:
+				all[j++] = cmdline[i++];
+				break;
+
+		}
+	}
+}
+
 
 /*==================================================================================*
  * void va_start:																	*
@@ -16,15 +83,15 @@ void va_start( void);
  * returns: 																		*
  *      --																			*
  *==================================================================================*/
- 
-void va_start( void) 
-{ 
+
+void va_start( void)
+{
 	char *p = *(char **) &evnt.buff[3];
 
 	if( p)
 	{
-		p = AvStrfmt( 1, p);
-		
+		p = ParseArgs( p );
+
 		/* yes, I know, it's strange to reput the menubar here
 		but if we don't do it, on MagiC, the menubar is not active after
 		a VA_START :( */
