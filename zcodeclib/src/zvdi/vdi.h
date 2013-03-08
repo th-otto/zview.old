06df7ec42942203fa65f24407614e67891c26719
Miro Kropáček <miro.kropacek@gmail.com>
2013-03-08 10:28:45 +0000

    Tag each release.

--- zcodeclib/src/zvdi/vdi.h
+++
@@ -1,7 +0,0 @@
-extern int16 	pixel_val[256];
-extern uint32 	cube216[216];
-extern uint32	graymap[32];
-
-extern int16 vdi_init( void);
-extern int16 planar, vdi_handle, nplanes, vdi_work_in[10], vdi_work_out[57];
-
--- ztask/taskman.h
+++
@@ -1,7 +0,0 @@
-extern void taskman_dialog( void);
-extern void taskman_timer( void);
-extern WINDOW 	*taskman_win;
-
-
-
-
