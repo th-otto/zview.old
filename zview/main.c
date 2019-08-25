89c59df839da88fe1160adec62208d76593ec932
Thorsten Otto <admin@tho-otto.de>
2019-08-25 10:07:56 +0200

    Release version 1.0.2

--- zview/main.c
+++ zview/main.c
@@ -13,7 +13,7 @@
 #include "catalog/catalog.h"
 
 char const program_version[] = " zView 1.0.2 ";
-char const program_date[] = "August 18, 2019";
+char const program_date[] = "August 25, 2019";
 
 #ifndef VA_START
 #define VA_START 0x4711
