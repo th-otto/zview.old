aef6dabda8a9813742acfc160a440ff73077cc24
Thorsten Otto <admin@tho-otto.de>
2019-09-27 10:10:10 +0200

    Fix a bug in the build script

--- .travis/build.sh
+++ .travis/build.sh
@@ -12,20 +12,15 @@ mkdir -p "${OUT}"
 
 unset CC CXX
 
-CPU_CFLAGS_000=-m68000
-CPU_CFLAGS_020=-m68020-60
-CPU_CFLAGS_v4e=-mcpu=5475
-
 ARCHIVES=
 
 cd zview
-for flavour in 000 v4e 020; do
-	: eval CPU=\${CPU_CFLAGS_$flavour}
+for CPU in 000 v4e 020; do
 	make clean
 	make CPU=$CPU || exit 1
 	make CPU=$CPU dist || exit 1
 
-	ARCHIVE="${PROJECT_LOWER}-${flavour}${ATAG}.zip"
+	ARCHIVE="${PROJECT_LOWER}-${CPU}${ATAG}.zip"
 	cd "_dist"
 	zip -r "${OUT}/${ARCHIVE}" .
 	ARCHIVES="$ARCHIVES $ARCHIVE"
